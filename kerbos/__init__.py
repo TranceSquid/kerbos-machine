47a5eb5c369658d172f55496318bfd66aa5ec994
Joey Pereira <joey@pereira.io>
2015-07-27 22:44:17 -0400

    changes for prod, added more non-blocking functionality

--- kerbos/__init__.py
+++ kerbos/__init__.py
@@ -6,6 +6,24 @@ from flask_sqlalchemy import SQLAlchemy
 import kerbos.shared as Shared
 
 
+# Optionally, set up psycopg2 & SQLAlchemy to be greenlet-friendly.
+# Note: psycogreen does not really monkey patch psycopg2 in the
+# manner that gevent monkey patches socket.
+#
+if "PSYCOGREEN" in os.environ:
+
+    # Do our monkey patching
+    #
+    from gevent.monkey import patch_all
+    patch_all()
+    from psycogreen.gevent import patch_psycopg
+    patch_psycopg()
+
+    using_gevent = True
+else:
+    using_gevent = False
+
+
 Shared.APP = Flask(__name__)
 app = Shared.APP
 
@@ -13,6 +31,17 @@ CONFIG_PATH = 'kerbos.config.' + os.environ.get('APP_SETTINGS', 'DevelopmentConf
 app.config.from_object(CONFIG_PATH)
 
 Shared.DB = SQLAlchemy(app)
+
+if using_gevent:
+
+    # Assuming that gevent monkey patched the builtin
+    # threading library, we're likely good to use
+    # SQLAlchemy's QueuePool, which is the default
+    # pool class.  However, we need to make it use
+    # threadlocal connections
+    #
+    #
+    Shared.DB.engine.pool._use_threadlocal = True
 
 @app.route('/')
 def hello():
--- run.sh
+++
@@ -1,6 +0,0 @@
-if [ $1 = 'env' ]; then
-    source ./.env/bin/activate
-fi
-
-echo "Usage:"
-echo "  run.sh env => enters virtualenv"
